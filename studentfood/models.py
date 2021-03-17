61076126e123f0218f190c4e4e15dd64f4634a78
ISR404 <1.6.2mine@gmail.com>
2021-03-18 00:07:56 +0700

    Первые модели

--- studentfood/models.py
+++ studentfood/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    recipe_name = models.CharField('Название', max_length=40)
+    pub_date = models.DateTimeField('Дата публикации')
+    description = models.TextField('Описание')
+    mark = 0
+
+    def __str__(self):
+        return self.recipe_name
+
+
+class User(models.Model):
+    name = models.CharField('Никнейм', max_length=10)
+    photo = models.ImageField('Фото')
+    email = models.EmailField('Почта')
+
+
+class Comment(models.Model):
+    text = models.TextField('Текст комментария')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+
+
--- studentfood/views.py
+++ studentfood/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Recipe, User, Comment
+
 
 # Create your views here.
+
+def index(request):
+    recipes_list = Recipe.objects.all()
+    output = ', '.join([r.recipe_name for r in recipes_list])
+    return HttpResponse(output)
