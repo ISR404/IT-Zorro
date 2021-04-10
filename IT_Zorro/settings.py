85c558fb0a20fdcc48b427a47c97635189004ca6
ISR404 <1.6.2mine@gmail.com>
2021-04-10 23:40:50 +0700

    Пересоздан пользователь. Рецепты и пользователи зарегистрированы в админ-панели

--- IT_Zorro/settings.py
+++ IT_Zorro/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
+
+AUTH_USER_MODEL = 'studentfood.User'
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- studentfood/models.py
+++ studentfood/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+global_category = [
+    'Первые блюда',
+    'Вторые блюда',
+    'Закуски/Салаты',
+    'Сладкое/Десерты'
+]
 
 
 class Recipe(models.Model):
@@ -11,35 +18,20 @@ class Recipe(models.Model):
     description = models.TextField('Описание')
     mark = 0
     price = models.IntegerField('Примерная стоимость')
+    category = models.CharField('Категория', max_length=30)
 
     def __str__(self):
         return self.recipe_name
     # не забыть добавить ценник для рецепта!
 
 
-class FirstRecipe(Recipe):
-    pass
-
-
-class SecondRecipe(Recipe):
-    pass
-
-
-class EasyRecipe(Recipe):
-    pass
-
-
-class DesertRecipe(Recipe):
-    pass
-    
-
-class SiteUser(User):
+class User(AbstractUser):
     photo = models.ImageField('Фото')
 
 
 class Comment(models.Model):
     text = models.TextField('Текст комментария')
-    user = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
 
