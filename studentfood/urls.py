959c52e89dc016eb632379f19726c4e41551f4fd
ISR404 <1.6.2mine@gmail.com>
2021-04-22 00:20:16 +0700

    Пытаюсь понять как Димин HTML работает. Немного расределил шаблоны по категориям

--- studentfood/urls.py
+++ studentfood/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'studentfood'
 urlpatterns = [
     path('catalog/', views.index, name='index'),
-    path('<int:recipe_id>/', views.detail, name='detail')
+    path('<int:recipe_id>/', views.detail, name='detail'),
+    path('register/', views.register, name='register'),
+    path('auth/', views.auth, name='auth'),
+    path('product', views.product, name='product'),
+    path('profile/', views.profile, name='profile'),
 ]
--- studentfood/views.py
+++ studentfood/views.py
@@ -9,9 +9,25 @@ from django.http import Http404
 def index(request):
     recipes_list = Recipe.objects.order_by('-pub_date')
     context = {'recipes_list': recipes_list}
-    return render(request, 'studentfood/backend-temp/test_schema.html', context)
+    return render(request, 'studentfood/html/main.html', context)
 
 
 def detail(request, recipe_id):
     recipe_detail = get_object_or_404(Recipe, pk=recipe_id)
     return render(request, 'studentfood/backend-temp/detail_test.html', {'recipe_detail': recipe_detail})
+
+
+def register(request):
+    return render(request, 'profiles/register.html')
+
+
+def auth(request):
+    return render(request, '')
+
+
+def product(request):
+    return render(request, 'studentfood/html/product.html')
+
+
+def profile(request):
+    return render(request, 'profiles/profile.html')
