ac19c199e74665527f3bd75742a0cfd153d115bd
ISR404 <1.6.2mine@gmail.com>
2021-04-22 21:36:29 +0700

    Допилил функцию создания рецепта пользователем. При переносе моделей необходимо удалить старые таблицы БД кроме sqlite_master и sqlite_sequence

--- studentfood/models.py
+++ studentfood/models.py
@@ -14,6 +14,14 @@ global_category = [
 # подписать поля
 
 
+class User(AbstractUser):
+    photo = models.ImageField('Фото')  # список всех полей есть в файле миграции.
+
+    def create_recipe(self, recipe_name, description, price, category):  # по идее параметры для функции должны браться
+        #  из полей фронта, а потом на их основе выполняется функция создания и привязки рецепта к юзеру
+        self.recipe_set.create(recipe_name=recipe_name, description=description, price=price, category=category, pub_date=timezone.now())
+
+
 class Recipe(models.Model):  # рецепт
     recipe_name = models.CharField('Название', max_length=40)  # название рецепта
     pub_date = models.DateTimeField('Дата публикации')  # дата публикация
@@ -21,20 +29,13 @@ class Recipe(models.Model):  # рецепт
     mark = 0  # оценка рецепта
     price = models.IntegerField('Примерная стоимость')
     category = models.CharField('Категория', max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):  # при запросе класса выводит название рецепта
         return self.recipe_name
 
     def calculate_mark(self):  # реализовать оценку
         pass
-
-
-class User(AbstractUser):
-    photo = models.ImageField('Фото')  # список всех полей есть в файле миграции.
-
-    def create_recipe(self, recipe_name, description, price, category):  # по идее параметры для функции должны браться
-        #  из полей фронта, а потом на их основе выполняется функция создания и привязки рецепта к юзеру
-        self.recipe_set.create(recipe_name=recipe_name, description=description, price=price, category=category)
 
 
 class Comment(models.Model):
