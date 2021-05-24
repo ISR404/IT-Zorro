ba616b86ba3a98f12b8a03a0b781ec6222b319b7
ISR404 <1.6.2mine@gmail.com>
2021-05-24 18:37:11 +0700

    Реализован поиск

--- studentfood/models.py
+++ studentfood/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    photo = models.ImageField('Фото')  # список всех полей есть в файле миграции.
+    photo = models.ImageField('Фото', default='media/user_img/no_photo.jpg', upload_to='user_img')  # установлена стандартная фотография. пользовательские фото будут загружены в media/user_img
 
     def create_recipe(self, recipe_name, description, price, category):  # по идее параметры для функции должны браться
         #  из полей фронта, а потом на их основе выполняется функция создания и привязки рецепта к юзеру
@@ -17,11 +17,11 @@ class User(AbstractUser):
 
 class Recipe(models.Model):  # рецепт
     recipe_name = models.CharField('Название', max_length=40)  # название рецепта
-    pub_date = models.DateTimeField('Дата публикации')  # дата публикация
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)  # дата публикация
     description = models.TextField('Описание')  # описание рецепта
     avg_mark = 0  # оценка рецепта
     count_of_submits = 0  # количество людей, поставивших оценку
-    price = models.IntegerField('Примерная стоимость')
+    price = models.IntegerField('Примерная стоимость', null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     GLOBAL_CATEGORY = [
         ('first', 'Первые блюда'),
@@ -30,6 +30,7 @@ class Recipe(models.Model):  # рецепт
         ('sweet', 'Сладкое/Десерты'),
     ]
     category = models.CharField('Категория', choices=GLOBAL_CATEGORY, max_length=16, blank=True)
+    photo = models.ImageField('Фото рецепта', null=True, blank=True, upload_to='recipe_img')  # фото будут храниться по пути /media/recipe_img (не забудь создать эти папки перед тестом)
 
     def __str__(self):  # при запросе класса выводит название рецепта
         return self.recipe_name
@@ -40,15 +41,24 @@ class Recipe(models.Model):  # рецепт
     def set_mark(self):  # проверить авторизацию пользователя
 
         pass
-    # def leave_comment(self, text):
-        # user = User.objects.get()
-        # self.comment_set.create(text=text)
 
 
 class Comment(models.Model):
-    text = models.TextField('Текст комментария')
+    text = models.CharField('Текст комментария', max_length=1500)
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # связь комментария с пользователем
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)  # связь комментария с рецептом
+    pub_date = models.DateTimeField('Опубликовано', default=timezone.now)
 
+    """
+    def leave_comment(self):
+        com = Comment()
+        recipe = Recipe.objects.get(pk=...)
+        user = User.objects.get(...)  # request во view
+        text = 'some text'
+        com.recipe = recipe
+        com.user = user
+        com.text = text  # на будущее
+        com.save()
+    """
 
 
--- studentfood/views.py
+++ studentfood/views.py
@@ -1,25 +1,89 @@
 from django.shortcuts import render, get_object_or_404
-# from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.utils import timezone
 from .models import Recipe, User, Comment
 from django.http import Http404
+from .forms import CommentForm, RecipeForm
 
 
 # Create your views here.
 
-def main(request):  # лист рецептов,
-    recipes_list = Recipe.objects.order_by('-pub_date')
-    context = {'recipes_list': recipes_list}
+def main(request, use_filter=''):  # лист рецептов,
+    null_recipe = Recipe()
+    raw_category = null_recipe.GLOBAL_CATEGORY
+    site_category = []
+    first_arg = []
+    for elem in raw_category:
+        site_category.append(elem[1])
+        first_arg.append(elem[0])
+    if use_filter == '':
+        recipes_list = Recipe.objects.order_by('-pub_date')
+    else:
+        recipes_list = Recipe.objects.get(category__iexact=use_filter)
+    context = {'recipes_list': recipes_list,
+               'site_category': site_category,
+               'first_arg': first_arg,
+               'raw_category': raw_category
+               }
     return render(request, 'studentfood/html/main.html', context)
 
 
 def detail(request, recipe_id):  # объект (написать список полей)
     recipe_detail = get_object_or_404(Recipe, pk=recipe_id)
-    return render(request, 'studentfood/html/product.html', {'recipe_detail': recipe_detail})
+    comment_list = recipe_detail.comment_set.order_by('pub_date')
+    if request.user.is_authenticated:
+        if request.method == 'GET':
+            comment_form = CommentForm()
+
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            post_comment = Comment()
+            if comment_form.is_valid():
+                post_comment.text = comment_form.cleaned_data.get("text")
+                post_comment.recipe = get_object_or_404(Recipe, pk=recipe_id)
+                post_comment.user = request.user
+                post_comment.save()
+                return render(request, 'studentfood/html/product.html', {'recipe_detail': recipe_detail,
+                                                                         'comment_form': comment_form,
+                                                                         'comment_list': comment_list})
+
+    return render(request, 'studentfood/html/product.html', {'recipe_detail': recipe_detail,
+                                                             'comment_list': comment_list})
+
+
+def register(request):  # реализация Димы
+    return render(request, 'profiles/register.html')
 
 
 def profile(request):  # пользовательские данные (при переходе возвращает объект пользователя)
-    return render(request, 'studentfood/html/profiles/profile.html')
+    return render(request, 'profiles/profile.html')
 
 
-def category_filter(request):
-    pass
+def create_recipe(request):
+    if request.user.is_authenticated:
+        if request.method == 'GET':
+            recipe_form = RecipeForm()
+            recipe_form.user = request.user
+            return render(request, 'studentfood/backend-temp/test_recipe_create.html', {'recipe_form': recipe_form,
+                                                                                        'request': request})
+        if request.method == 'POST':
+            recipe_form = RecipeForm(request.POST, request.FILES)
+            post_recipe = Recipe()
+            if recipe_form.is_valid():
+                if 'photo' in request.FILES:  # проверка на то, есть ли в реквесте фотки
+                    post_recipe.photo = request.FILES['photo']  # если есть, то присваиваем сохраняемому рецепту
+                post_recipe.recipe_name = recipe_form.cleaned_data.get("recipe_name")
+                post_recipe.description = recipe_form.cleaned_data.get("description")
+                post_recipe.price = recipe_form.cleaned_data.get("price")
+                post_recipe.user = request.user
+                post_recipe.category = recipe_form.cleaned_data.get("category")
+                post_recipe.save()
+                return render(request, 'studentfood/backend-temp/test_recipe_create.html')
+            else:
+                return HttpResponse('Ваша форма недействительна!')
+    else:
+        return HttpResponse('Сначала нужно авторизироваться!')
+
+
+
