d9e0d32b4d3fb7a131a5675e7e2346e9475b0023
CoolFice <dima-mayer@list.ru>
2021-05-25 22:57:34 +0700

    Обновление

--- studentfood/models.py
+++ studentfood/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
@@ -9,7 +10,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     photo = models.ImageField('Фото', default='user_img/no_photo.jpg', upload_to='user_img')  # установлена стандартная фотография. пользовательские фото будут загружены в media/user_img
-
 
     def create_recipe(self, recipe_name, description, price, category):  # по идее параметры для функции должны браться
         #  из полей фронта, а потом на их основе выполняется функция создания и привязки рецепта к юзеру
@@ -25,8 +25,6 @@ class Recipe(models.Model):  # рецепт
     recipe_name = models.CharField('Название', max_length=40)  # название рецепта
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)  # дата публикация
     description = models.TextField('Описание')  # описание рецепта
-    avg_mark = 0  # оценка рецепта
-    count_of_submits = 0  # количество людей, поставивших оценку
     price = models.IntegerField('Примерная стоимость', null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     GLOBAL_CATEGORY = [
@@ -46,9 +44,18 @@ class Recipe(models.Model):  # рецепт
         if self.photo and hasattr(self.photo, 'url'):
             return self.photo.url
 
-    # def calculate_mark(self):  # реализовать оценку
-    #     pass
-    #
+    def calculate_mark(self):  # реализовать оценку
+        max_marks = self.mark_set.all()
+        sum_marks = 0
+        for get_mark in max_marks:
+            sum_marks += get_mark.mark_value
+        if len(max_marks) == 0:
+            avg_mark = 0
+        else:
+            avg_mark = sum_marks/len(max_marks)
+        return avg_mark
+
+
     # def set_mark(self):  # проверить авторизацию пользователя
     #
     #     pass
@@ -60,14 +67,21 @@ class Comment(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)  # связь комментария с рецептом
     pub_date = models.DateTimeField('Опубликовано', default=timezone.now)
 
-    """
-    def leave_comment(self):
-        com = Comment()
-        recipe = Recipe.objects.get(pk=...)
-        user = User.objects.get(...)  # request во view
-        text = 'some text'
-        com.recipe = recipe
-        com.user = user
-        com.text = text  # на будущее
-        com.save()
-    """
+
+class Mark(models.Model):
+    USER_MARK_SET = [
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+    ]
+    mark_value = models.FloatField(choices=USER_MARK_SET, validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('recipe', 'user')
+
+    # def __str__(self):
+    #     return str('Оценка к рецепту ' + self.recipe + ' пользоветелем ' + self.user)
